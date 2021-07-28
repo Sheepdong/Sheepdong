44be374f8fb888dbef82135d1aa3eac2b8d5bf0b
Sheepdong <donghyeon9489@ajou.ac.kr>
2021-07-28 23:33:29 +0900

    coefficient arrange

---
+++ 계수 정렬.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+#define MAX_VALUE 10001
+
+int n, m;
+int a[MAX_VALUE];
+
+int main(void) {
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &m);
+		a[m]++;
+	}
+	for (int i = 0; i < MAX_VALUE; i++) {
+		while (a[i] != 0) { printf("%d ", i); a[i]--; }
+	}
+	system("pause");
+}
